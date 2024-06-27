9fb10c15086e33df096552134c1b7b39ebb433b4
zacharybaca <zachary.baca@msn.com>
2024-06-26 21:02:56 -0500

    complete

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -1,13 +1,19 @@
+/* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import "../Friend.css";
 
 function Friend(props) {
     return (
-        <div>
-            <h1>Name: {props.name}</h1>
-            <h1>Age: {props.age}</h1>
-            
+        <div id="friend-card">
+            <h1 className="card-info">Name: {props.name}</h1>
+            <h1 className="card-info">Age: {props.age}</h1>
+                <ul>
+                    {props.pets.map((pet) => {
+                        return (
+                        <div id="list-items"><li>Pet Name: {pet.name}</li><li>Pet Breed: {pet.breed}</li></div>
+                    )})}
+                </ul>
         </div>
     )
 }
